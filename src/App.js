1f03fd52b90c7e5d0bb31641c4655d428b33d226
Oscar G. Rodrigues <g.rodrigues.oscar@gmail.com>
2018-10-09 16:04:54 +0200

    ajout de MusicPlayer et routing du component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
 import ListSimilarArtists from './ListSimilarArtists';
 import ArtistGetInfo from './ArtistGetInfo';
 import TopTrack from './TopTrack';
+import MusicPlayer from './MusicPlayer'
 import './App.css';
 
 class App extends Component {
@@ -25,10 +26,12 @@ class App extends Component {
             <NavLink exact className="navbarlink" to="/listsimilarartists"> ListSimilarArtists </NavLink>
             <NavLink className="navbarlink" to="/artistgetinfo"> ArtistGetInfo </NavLink>
             <NavLink className="navbarlink" to="/toptrack"> TopTrack </NavLink>
+            <NavLink className="navbarlink" to="/musicplayer"> MusicPlayer </NavLink>
             <Switch>
                 <Route exact path="/listsimilarartists" component={ListSimilarArtists} />
                 <Route path="/artistgetinfo" component={ArtistGetInfo} />
                 <Route path="/toptrack" component={TopTrack} />
+                <Route path="/musicplayer" component={MusicPlayer} />
             </Switch>
           </header>
         </div>
