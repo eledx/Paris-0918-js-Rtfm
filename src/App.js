db40b05c9a28c3cc6b6d4ca706ff581eba9da3ba
eledx <eleonore.dupoix@gmail.com>
2018-10-09 18:53:55 +0200

    Autocopletion.js content moved into App.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,77 @@ import ListSimilarArtists from './ListSimilarArtists';
 import ArtistGetInfo from './ArtistGetInfo';
 import TopTrack from './TopTrack';
 import MusicPlayer from './MusicPlayer'
-import Autocompletion from './Autocompletion';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      userInput: '',
+      finalSearch: '',
+      suggestions: [],
+      similarArtists: []
+    }
+  }
+
+  // Afficher nom d'artiste dans searchBar & appeler l'autocomplétion
+  searchBarDisplay = (event) => {
+    this.setState({userInput: event.target.value})
+    this.requestAutocompletion(event.target.value)
+  }
+
+  //  Appel de l'API : méthode d'autocomplétion
+  requestAutocompletion = (artist) => {
+    fetch(`http://audioscrobbler.com/2.0/?method=artist.search&artist=${artist}&limit=5&api_key=af05581a38f69802ba020346115c8834&format=json`)
+    .then(resp => resp.json())
+    .then(resp => this.setState({suggestions : resp.results.artistmatches.artist}))
+    }
+
+  // Instructions à exécuter sur le clic du bouton submit
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({finalSearch: event.target[0].value})
+    this.setState({userInput: event.target[0].value})
+    console.log(this.state)
+  }
+
+  // Instructions à exécuter sur le clic d'un nom d'artiste
+  handleArtistClick = (event) => {
+    this.setState({finalSearch: event.target.innerText})
+    this.setState({userInput: event.target.innerText})
+    console.log('artisClick' , this.state)
+  }
+
+
 
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <header className="App-header">
+            <header className="App-header">
+            <form onSubmit={this.handleSubmit}>
+              <input 
+              type="text" 
+              placeholder="Votre artiste"
+              value={this.state.userInput}
+              onChange={this.searchBarDisplay}/>
+              <button>Search</button>
+            </form>
+            <div>
+            {this.state.suggestions.map(
+              (element, i) => <p key={i} onClick={this.handleArtistClick}>{element.name}</p>
+            )}
+              </div>
             <NavLink exact className="navbarlink" to="/listsimilarartists"> ListSimilarArtists </NavLink>
             <NavLink className="navbarlink" to="/artistgetinfo"> ArtistGetInfo </NavLink>
             <NavLink className="navbarlink" to="/toptrack"> TopTrack </NavLink>
             <NavLink className="navbarlink" to="/musicplayer"> MusicPlayer </NavLink>
-            <NavLink className="navbarlink" to="/autocompletion"> SearchBar </NavLink>
             <Switch>
                 <Route exact path="/listsimilarartists" component={ListSimilarArtists} />
                 <Route path="/artistgetinfo" component={ArtistGetInfo} />
                 <Route path="/toptrack" component={TopTrack} />
                 <Route path="/musicplayer" component={MusicPlayer} />
-                <Route path="/autocompletion" component={Autocompletion} />
             </Switch>
           </header>
         </div>
